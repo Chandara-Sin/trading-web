4423b5883688bed0a9ac8be025c1a0d2eedf9fab
Chandara-Sin <cdsin68@gmail.com>
2023-01-27 20:54:24 +0700

    feat: init tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
+import Navbar from "../components/Navbar.vue";
 </script>
 
 <template>
+  <!-- <Navbar /> -->
   <main>
     <TheWelcome />
   </main>
