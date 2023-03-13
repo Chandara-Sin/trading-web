8a3fc36cae56aeba546601a239c291418d19f92b
Chandara-Sin <cdsin68@gmail.com>
2023-03-13 22:49:12 +0700

    refactor: add stockcard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Navbar from "../components/Navbar.vue";
 import Banner from "../components/Banner.vue";
+import StockCard from "../components/StockCard.vue";
 </script>
 
 <template>
   <main>
     <Navbar />
     <Banner />
+    <StockCard />
   </main>
 </template>
